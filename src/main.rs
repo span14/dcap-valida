673a8065a4687bb2d6d4d99c2e203997b6c156d2
Marty Stumpf <thealmartyblog@gmail.com>
2024-10-29 11:55:10 -0700

    Update package name of `valida-rs`. (#9)
    * Update package name.
    
    * Update cargo lock and fn names.
    
    ---------
    
    Co-authored-by: thealmarty <“thealmartyblog@gmail.com”>

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 #![no_main]
 
-entrypoint::entrypoint!(main);
+valida_rs::entrypoint!(main);
 
 pub fn main() {
-    entrypoint::io::println("Please enter a number from 0 to 46:");
+    valida_rs::io::println("Please enter a number from 0 to 46:");
     // Read a line from stdin and parse it as an u8.
     let n = loop {
-        match entrypoint::io::read_line::<u8>() {
+        match valida_rs::io::read_line::<u8>() {
             Ok(num) => break num,
             Err(e) => {
-                entrypoint::io::println(&format!("Error reading input: {}. Please try again:", e));
+                valida_rs::io::println(&format!("Error reading input: {}. Please try again:", e));
             }
         }
     };
     // n that is larger than 46 will overflow the u32 type.
     if n > 46 {
-        entrypoint::io::println("Error: n is too large. Please enter a number no more than 46.");
+        valida_rs::io::println("Error: n is too large. Please enter a number no more than 46.");
         return;
     }
     let mut a: u32 = 0;
@@ -27,7 +27,7 @@ pub fn main() {
         b = sum;
     }
 
-    entrypoint::io::println(&n.to_string());
-    entrypoint::io::println("-th fibonacci number is:");
-    entrypoint::io::println(&b.to_string());
+    valida_rs::io::println(&n.to_string());
+    valida_rs::io::println("-th fibonacci number is:");
+    valida_rs::io::println(&b.to_string());
 }
